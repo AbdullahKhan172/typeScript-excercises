9c7be01333ce5772d004e81b87eaaf80d1cc710b
Abdullah Khan <iiabdullahii.khan@gmail.com>
2023-02-11 22:32:59 +0500

    Q3 Done

---
+++ Question2/question2.js
@@ -0,0 +1,3 @@
+var Name1 = "aBdullah";
+console.log(Name1.toUpperCase());
+console.log(Name1.toLowerCase());
---
+++ Question2/question2.ts
@@ -0,0 +1,4 @@
+let Name1: string = "aBdullah";
+
+console.log(Name1.toUpperCase());
+console.log(Name1.toLowerCase());
